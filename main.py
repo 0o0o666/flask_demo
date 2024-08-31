9fa6ac9e51f27ef1738ae9dceee149b3e7827392
scc <scc@gmail.com>
2024-08-31 12:04:56 +0800

    第二次書架完成

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 print("__name__")
 
 app = Flask(__name__)
-books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}
 
 
 @app.route("/sum/x=<x>&y=<y>")
@@ -29,12 +45,19 @@ def index():
     today = datetime.now()
     print(today)
     print("123")
-    return f"<h1>Hello Flask!<br>{today}</hz1>"
+    name = "aaa"
+    # return f"<h1>Hello Flask!<br>{today}</hz1>"
+    return render_template("index.html", date=today, name=name)
 
 
 @app.route("/books")
 def show_books():
-    return books
+    # return books
+    print(books)
+
+    for key in books:
+        print(books[key])
+    return render_template("books.html", books=books)
 
 
 @app.route("/bmi/name=<name>&weight=<int:weight>&height=<int:height>")
