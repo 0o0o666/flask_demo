8d3b76e13c2e54be1a9e032b25305bc48936453e
scc <scc@gmail.com>
2024-09-21 10:11:47 +0800

    0921第二次

--- scrape.py
+++ scrape.py
@@ -25,14 +25,45 @@ def sripe_stocks():
     return None
 
 
+def convert_value(value):
+    try:
+        return eval(value)
+    except:
+        return None
+
+
+def get_pm25_deta():
+    datas = requests.get(url).json()["records"]
+    df = pd.DataFrame(datas)
+    # 將非正常數值轉換成None
+    df["pm25"] = df["pm25"].apply(convert_value)
+    # 移除有None的數據
+    df = df.dropna()
+
+    return df
+
+
+def scrape_six_pm25():
+    six_countys = ["臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市"]
+    pm25 = []
+    try:
+        df = get_pm25_deta()
+        for county in six_countys:
+            avg_pm25 = df.groupby("county").get_group(county)["pm25"].mean()
+            pm25.append(avg_pm25)
+
+        columns = six_countys
+        values = pm25
+
+        return columns, values
+    except Exception as e:
+        print(e)
+    return None, 404
+
+
 def scrape_pm25(sort=False, ascending=True):
     try:
-        datas = requests.get(url).json()["records"]
-        df = pd.DataFrame(datas)
-        # 將非正常數值轉換成None
-        df["pm25"] = df["pm25"].apply(convert_value())
-        # 移除有None的數據
-        df = df.dropna()
+        df = get_pm25_deta()
         if sort:
             df = df.sort_values("pm25", ascending=ascending)
 
@@ -43,6 +74,12 @@ def scrape_pm25(sort=False, ascending=True):
     except Exception as e:
         print(e)
     return None, 404
+
+
+def get_six_pm25_json():
+    columns, values = scrape_six_pm25()
+    json_data = {"site": columns, "pm25": values}
+    return json_data
 
 
 def get_pm25_json():
@@ -56,13 +93,4 @@ def get_pm25_json():
 
 
 if __name__ == "__main__":
-    print(sripe_stocks())
-    print(scrape_pm25(sort=True, ascending=False))
-    print(get_pm25_json())
-
-
-def convert_value(value):
-    try:
-        return eval(value)
-    except:
-        return None
+    print(scrape_six_pm25())
